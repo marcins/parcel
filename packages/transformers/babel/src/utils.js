b00491a315c72f69f36e9cb3ef2f0e645c3388bf
thebriando <bdo@atlassian.com>
2023-04-03 12:37:17 -0700

    Remove custom node getMinVersion function

--- packages/transformers/babel/src/utils.js
+++ packages/transformers/babel/src/utils.js
@@ -62,7 +62,7 @@ export function enginesToBabelTargets(env: Environment): BabelTargets {
     } else {
       invariant(typeof engineValue === 'string');
       if (!TargetNames.hasOwnProperty(engineName)) continue;
-      let minVersion = getMinSemver(engineValue);
+      let minVersion = semver.minVersion(engineValue)?.toString();
       targets[engineName] = minVersion ?? engineValue;
     }
   }
@@ -83,18 +83,4 @@ export function enginesToBabelTargets(env: Environment): BabelTargets {
   }
 
   return targets;
-}
-
-// ATLASSIAN: reverting this PR for now to unblock builds
-// https://github.com/parcel-bundler/parcel/pull/7334
-// TODO: Replace with `minVersion` (https://github.com/npm/node-semver#ranges-1)
-//       once semver has been upgraded across Parcel.
-export function getMinSemver(version: string): ?string {
-  try {
-    let range = new semver.Range(version);
-    let sorted = range.set.sort((a, b) => a[0].semver.compare(b[0].semver));
-    return sorted[0][0].semver.version;
-  } catch (err) {
-    return null;
-  }
 }
