0f9c7b600bd5ee66a61b2860fc20ad3daeb482b5
Marcin Szczepanski <mszczepanski@atlassian.com>
2023-06-14 17:23:23 +1000

    Add failing integration test for lazy compilation

---
+++ packages/core/integration-tests/test/lazy-compile.js
@@ -0,0 +1,84 @@
+import assert from 'assert';
+import path from 'path';
+import {
+  bundler,
+  outputFS,
+  distDir,
+  getNextBuild,
+  assertBundles,
+  removeDistDirectory,
+} from '@parcel/test-utils';
+
+const findBundle = (bundleGraph, nameRegex) => {
+  return bundleGraph.getBundles().find(b => nameRegex.test(b.name));
+};
+
+const distDirIncludes = async matches => {
+  const files = await outputFS.readdir(distDir);
+  for (const match of matches) {
+    if (typeof match === 'string') {
+      if (!files.some(file => file === match)) {
+        throw new Error(
+          `No file matching ${match} was found in ${files.join(', ')}`,
+        );
+      }
+    } else {
+      if (!files.some(file => match.test(file))) {
+        throw new Error(
+          `No file matching ${match} was found in ${files.join(', ')}`,
+        );
+      }
+    }
+  }
+  return true;
+};
+
+describe('lazy compile', function () {
+  it('should lazy compile', async function () {
+    const b = await bundler(
+      path.join(__dirname, '/integration/lazy-compile/index.js'),
+      {
+        shouldBuildLazily: true,
+        mode: 'development',
+        shouldContentHash: false,
+      },
+    );
+
+    await removeDistDirectory();
+
+    const subscription = await b.watch();
+    let result = await getNextBuild(b);
+
+    result = await result.requestBundle(
+      findBundle(result.bundleGraph, /index.js/),
+    );
+    result = await result.requestBundle(
+      findBundle(result.bundleGraph, /^lazy-1/),
+    );
+    result = await result.requestBundle(
+      findBundle(result.bundleGraph, /^lazy-2/),
+    );
+
+    // Expect the bundle graph to contain the whole nest of lazy from `lazy-1`, but not
+    // `parallel-lazy-1` which wasn't requested.
+    assertBundles(result.bundleGraph, [
+      {
+        assets: ['index.js', 'bundle-url.js', 'cacheLoader.js', 'js-loader.js'],
+      },
+      {
+        assets: ['lazy-1.js', 'esmodule-helpers.js'],
+      },
+      {
+        assets: ['lazy-2.js'],
+      },
+      {
+        assets: ['parallel-lazy-1.js'],
+      },
+    ]);
+
+    subscription.unsubscribe();
+
+    // Ensure the files match the bundle graph - lazy-2 should've been produced as it was requested
+    assert(await distDirIncludes(['index.js', /^lazy-1\./, /^lazy-2\./]));
+  });
+});
